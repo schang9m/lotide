34ee31661ee17415ffb42e52c5fa3f41bba4c1dd
Sampson Chang <sampson.chang@gmail.com>
2024-06-24 16:05:02 -0700

    fixed syntax

--- assertEqual.js
+++ assertEqual.js
@@ -2,9 +2,9 @@
 const args = process.argv;
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log("❤️️❤️️❤️️ Assertion Passed: " + actual + " === " + expected);
+    console.log(`❤️️❤️️❤️️ Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log("🖤🖤🖤Assertion Failed: " + actual + " !== " + expected);
+    console.log(`🖤🖤🖤Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
