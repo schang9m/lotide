142aa6fa4737c47766e7a0d2a0d3ee2ca9646488
Sampson Chang <sampson.chang@gmail.com>
2024-06-24 17:02:45 -0700

    added tail.js

---
+++ tail.js
@@ -0,0 +1,19 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`❤️️❤️️❤️️ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🖤🖤🖤Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const tail = function(arr) {
+  let newArr = [];
+  for (let i = 1; i < arr.length; i++) {
+    newArr.push(arr[i]);
+  }
+  return newArr;
+};
+// Test Case: Check the original array
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
