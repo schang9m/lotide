585f8e9fb6dcd7982d64a93c0d4df43c8b8f9a13
Sampson Chang <sampson.chang@gmail.com>
2024-06-25 16:51:41 -0700

    fixed some bug

--- eqArrays.js
+++ eqArrays.js
@@ -11,19 +11,21 @@ const assertEqual = function(actual, expected) {
 assertEqual(args[2] , args[3]);
 
 const eqArrays = function(arr1, arr2) {
-  //counter for arr2
-  let arr2Count = 0;
   let check = false;
-  for (let i = 0; i < arr1.length; i++){
-    //as arr[i]++ arr2Count also ++
-    if (arr1[i] !== arr2[arr2Count]) {
-      return false
-    }else{
-      check = true; 
-      arr2Count++;
+  if (arr1.length !== arr2.length){
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    } else {
+      check = true;
     }
   }
   return check;
 }
 
 assertEqual(eqArrays([1, 2, 3], [2, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3, 4], [2, 2, 3]), true);
+
