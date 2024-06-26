3a6d4b1f1228932c3ce6f7a268c8dd3da8733256
Sampson Chang <sampson.chang@gmail.com>
2024-06-25 18:30:09 -0700

    made changes to not use filter

--- without.js
+++ without.js
@@ -23,11 +23,25 @@ const assertArraysEqual = function(arr1, arr2) {
 const without = function(source, remove) {
   //compare source to remove,  and get rid of the ones that are the same
   //I can use filter
-  return source.filter(item => !remove.includes(item));
+  // return source.filter(item => !remove.includes(item));
   // const compare = function(item) {
   //   return !remove.includes(item)
   // };
   // let filterArr = source.filter(compare)
+  let newArr = [];
+  for(let i = 0; i < source.length; i++) {
+    let same = false;
+    for(let y = 0; y <remove.length; y++) {
+      if (source[i] === remove[y]) {
+        same = true;
+        break;
+      }
+    }
+    if(!same){
+      newArr.push(source[i]);
+    }
+  }
+  return newArr;
 };
 
 
