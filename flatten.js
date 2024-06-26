285dc8a78518df1bd2f391621706d07145132ca0
Sampson Chang <sampson.chang@gmail.com>
2024-06-25 22:00:08 -0700

    used concat to merge

--- flatten.js
+++ flatten.js
@@ -25,9 +25,10 @@ const flatten = function(arr) {
   let flatArr = [];
   for (let i = 0; i < arr.length; i++) {
     if (Array.isArray(arr[i])) {
-      for (let y = 0; y < arr[i].length; y++) {
-        flatArr.push(arr[i][y]);
-      }
+      // for (let y = 0; y < arr[i].length; y++) {
+      //   flatArr.push(arr[i][y]);
+      //}
+      flatArr = flatArr.concat(arr[i])
     } else {
       flatArr.push(arr[i]);
     }
