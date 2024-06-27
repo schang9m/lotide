02d3dd5fc75f7bac1b474d296da448cd5f0ce041
Sampson Chang <sampson.chang@gmail.com>
2024-06-27 16:39:26 -0700

    fixed eqArray code

--- eqArrays.js
+++ eqArrays.js
@@ -1,6 +1,6 @@
 // FUNCTION IMPLEMENTATION
 const args = process.argv;
-const assertEqual = function(actual, expected) {
+const assertEqual = function (actual, expected) {
   if (actual === expected) {
     console.log(`❤️️❤️️❤️️ Assertion Passed: ${actual} === ${expected}`);
   } else {
@@ -8,10 +8,7 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-assertEqual(args[2] , args[3]);
-
-const eqArrays = function(arr1, arr2) {
-  let check = false;
+const eqArrays = function (arr1, arr2) {
   if ((arr1.length || arr2.length) < 1) {
     return true;
   }
@@ -21,14 +18,12 @@ const eqArrays = function(arr1, arr2) {
   for (let i = 0; i < arr1.length; i++) {
     if (arr1[i] !== arr2[i]) {
       return false;
-    } else {
-      check = true;
     }
   }
-  return check;
+  return true;
 };
 
-assertEqual(eqArrays([1, 2, 3], [2, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [2, 2, 3]), false);
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3, 4], [2, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3, 4], [2, 2, 3]), false);
 
