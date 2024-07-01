9e4dba8a356fd9680575cd9f3f84bc5f2d863bfe
Sampson Chang <sampson.chang@gmail.com>
2024-07-01 12:16:27 -0700

    add more test on tail function

--- test/tailTest.js
+++ test/tailTest.js
@@ -5,8 +5,15 @@ const tail = require("../tail")
 describe("#tail", () => {
   it("Return Lighthouse first", () => {
     assert.deepEqual(tail(["Yo Yo", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"])
-  })
+  });
+
   it("Return the length -1", () => {
     assert.lengthOf(tail(["Yo Yo", "Lighthouse", "Labs"]), 2)
-  })
+  });
+  
+  it("make sure the original array was not altered by the tail function", () => {
+    const words = ["Yo Yo", "Lighthouse", "Labs"];
+    const result = tail(words);
+    assert.strictEqual(words.length, 3);
+  });
 })
